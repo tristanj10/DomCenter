f5a9dff055134549f1e7529561acc96c3550f14d
Tristan <tristan.jougier@gmail.com>
2016-11-12 14:24:27 +0100

    moc.php

--- moc/moc.php
+++ moc/moc.php
@@ -1,1 +1,10 @@
-efe
+if(isset($_GET['music']) && $_GET['music'] == "on"){
+	exec('mocp -S');
+	exec('mocp -c');
+	exec('mocp -a /var/www/moc/music');
+	exec('mocp -t shuffle');
+	exec('mocp -p');
+
+}else if(isset($_GET['music']) && $_GET['music'] == "off"){
+	exec('mocp -x');
+}
